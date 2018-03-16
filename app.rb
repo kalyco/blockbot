2d96cda4e527f0900ed8ae72bc616c686400528e
Kayla Comalli <Kayla.comalli@gmail.com>
2018-03-16 17:56:21 -0400

    initial commit. will probably need a refactor

---
+++ app.rb
@@ -0,0 +1,144 @@
+# encoding: utf-8
+require "sinatra"
+require "json"
+require "httparty"
+require "redis"
+require "dotenv"
+require "text"
+require "sanitize"
+require 'time'
+
+configure do
+	# Load .env vars
+	Dotenv.load
+
+  # Set up redis
+  case settings.environment
+  when :development
+    uri = URI.parse(ENV["LOCAL_REDIS_URL"])
+  when :production
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+  end
+  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
+
+# Handles the POST request made by the Slack Outgoing webhook
+# Params sent in the request:
+# 
+# token=abc123
+# team_id=T0001
+# channel_id=C123456
+# channel_name=test
+# timestamp=1355517523.000005
+# user_id=U123456
+# blocker=Steve
+# text=blockbot set blocker @person
+# trigger_word=blockbot
+# 
+
+post "/" do
+	begin
+		puts "[LOG] #{params}"
+		params[:text] = params[:text].sub(params[:trigger_word], "").strip
+		if params[:token] != ENV["OUTGOING_WEBHOOK_TOKEN"]
+      response = "Invalid token"
+    elsif params[:text].match(/^set blocker/i) 
+    	reponse =  set_blocker(params)
+    elsif params[:text].match(/^resolve/i) 
+    	reponse =  resolve_block(params)
+    elsif params[:text].match(/^ping blocker/i) 
+    	reponse =  ping_blocker(params)
+    elsif params[:text].match(/^help$/i)
+      response = respond_with_help
+    else 
+    	response = invalid_request
+	end
+	status 200
+	body json_response_for_slack(response)
+end
+
+# Puts together the json payload that needs to be sent back to Slack
+def json_response_for_slack(reply)
+  response = { text: reply, link_names: 1 }
+  response[:username] = ENV["BOT_USERNAME"] unless ENV["BOT_USERNAME"].nil?
+  response[:icon_emoji] = ENV["BOT_ICON"] unless ENV["BOT_ICON"].nil?
+  response.to_json
+end
+
+# Set a new blocker
+def set_blocker(params)
+	channel_id = params[:channel_id]
+	if existing_blocker
+		time_blocked = get_time_blocked
+		return "Can not create new issue. Current issue has been blocked by #{existing_blocker} for #{time_blocked}"
+	end	
+	if is_valid_blocker(params[:blocker])
+		$redis.set("blocked", params[:blocker])
+		$redis.set("time_blocked", time.Now)
+		$redis.set("blocker", params[:user_id])
+		$redis.set("team", params[:team_id])
+		return "Block created for team #{params[:team_id]}!"
+	end	
+end
+
+# Gets the existing blocker from redis
+def resolve_block()
+  blocker = $redis.get("blocker")
+  blocked = $redis.get("blocked")
+  time_blocked = $redis.get("time_blocked")
+  redis.set("blocker", nil)
+  redis.set("blocked", nil)
+  redis.set("time_blocked", nil)
+  return "#{blocker} resolved #{blocked}'s issue after #{time_blocked}"
+end
+
+# return time blocked
+def ping_blocker()
+	if existing_blocker
+		blocker = $redis.get("blocker")
+		blocked = $redis.get("blocked")
+		time_blocked = get_time_blocked
+		return "#{blocker} has been blocking #{blocked} for #{time_blocked}"
+	end
+	return "No existing blocks. Yay!"	
+end
+
+# Gets the existing blocker from redis
+def existing_blocker()
+  blocker = $redis.get("blocker")
+  if blocker.nil?
+  	return "No existing blocks. Yay!"	
+  end
+  return blocker
+end
+
+# Return total time on current block
+def get_time_blocked()
+	time_blocked = $redis.get("time_blocked")
+	now = time.Now
+  seconds_diff = (time_blocked - now).to_i.abs
+
+  hours = seconds_diff / 3600
+  seconds_diff -= hours * 3600
+
+  minutes = seconds_diff / 60
+  seconds_diff -= minutes * 60
+
+  seconds = seconds_diff
+
+  return "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+end
+
+def is_valid_blocker(blocker_name)
+	return $redis.exists(blocker_name)
+end	
+
+# Shows the help text.
+def respond_with_help
+  reply = <<help
+Type `#{ENV["BOT_USERNAME"]} set blocker [@slack_user]` to set a block.
+Type `#{ENV["BOT_USERNAME"]} resolve` to resolve an existing block.
+Type `#{ENV["BOT_USERNAME"]} ping blocker` to ping the blocker and display time blocked.
+help
+  reply
+end
